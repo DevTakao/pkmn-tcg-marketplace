982a19126435106647cd84d4d58a6d28b07d5713
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-02-04 03:02:30 +0630

    cart item alert on button

--- src/components/ViewCartButton.js
+++ src/components/ViewCartButton.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CartContext } from "../App";
 import "./ViewCartButton.css";
 
 export const ViewCartButton = ({ setOpenCart }) => {
+  const { cartItems, setCartItems } = useContext(CartContext);
+  const [alert, setAlert] = useState("");
+
+  useEffect(() => {
+    if (cartItems.length <= 9) {
+      setAlert(cartItems.length + "");
+    } else {
+      setAlert("9+");
+    }
+  }, [cartItems]);
+
   return (
     <div className="ViewCartButton">
       <button className="button" onClick={() => setOpenCart(true)}>
+        {!!parseInt(alert) && <span className="quantity-alert">{alert}</span>}
         View Cart
       </button>
     </div>
