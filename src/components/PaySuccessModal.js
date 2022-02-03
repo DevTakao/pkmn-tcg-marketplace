bd1e935c2fcbcc5ef791ca2a606bac781f55dc32
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-02-04 04:10:42 +0630

    payment tick icon

--- src/components/PaySuccessModal.js
+++ src/components/PaySuccessModal.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./PaySuccessModal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export const PaySuccessModal = ({ setPayDone }) => {
   return (
     <div className="PaySuccessModal">
       <div className="tick-icon-container">
-        <span className="tick-icon">✓</span>
+        <span className="tick-icon">
+          <FontAwesomeIcon icon={faCheck} className="fa-tick-icon" />
+        </span>
       </div>
       <p className="success-text">Payment Success!</p>
       <button className="close-modal-btn" onClick={() => setPayDone(false)}>
